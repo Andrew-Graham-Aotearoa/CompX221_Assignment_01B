d96035a01f9fadec95276ceaf0daa48b0566c65f
Andrew Graham <215948027+Andrew-Graham-Aotearoa@users.noreply.github.com>
2026-04-03 18:26:02 +1300

    Index Refactored to include Tailwind CSS. Config file for code completion in CDN environment

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+//Config sets up intelisense in the working file for tailwind
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // This tells the extension to look at your HTML files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
